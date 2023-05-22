a281a0342a1b78785d72d68a8231a19babe506f3
Le Thanh Tung <iamrobotdiy@gmail.com>
2023-05-22 21:48:00 +0700

    add seeders

--- backend/database/seeders/Categories.php
+++ backend/database/seeders/Categories.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class Categories extends Seeder
 {
@@ -12,6 +13,177 @@ class Categories extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = [
+            [
+                'id' => 1,
+                'name' => 'Cà Phê',
+                'parent_id' => 0,
+                'image_url' => 'https://minio.thecoffeehouse.com/image/admin/1678778483_ca-phe-uong-lien.png',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00'
+            ],
+            [
+                'id' => 2,
+                'name' => 'CloudFee',
+                'parent_id' => 0,
+                'image_url' => 'https://minio.thecoffeehouse.com/image/admin/1677724526_cloudfee-new.png',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00'
+            ],
+            [
+                'id' => 3,
+                'name' => 'ClouldTea',
+                'parent_id' => 0,
+                'image_url' => 'https://minio.thecoffeehouse.com/image/admin/1677724535_cloudtea.png',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00'
+            ],
+            [
+                'id' => 4,
+                'name' => 'Trà',
+                'parent_id' => 0,
+                'image_url' => 'https://minio.thecoffeehouse.com/image/admin/1677724564_tra-trai-cay.png',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00'
+            ],
+            [
+                'id' => 5,
+                'name' => 'Hi-Tea Heathy',
+                'parent_id' => 0,
+                'image_url' => 'https://minio.thecoffeehouse.com/image/admin/1677724550_hi-tea.png',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00'
+            ],
+            [
+                'id' => 6,
+                'name' => 'Bánh & Snack',
+                'parent_id' => 0,
+                'image_url' => 'https://minio.thecoffeehouse.com/image/admin/1677724490_banh.png',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00'
+            ],
+            [
+                'id' => 7,
+                'name' => 'Tại Nhà',
+                'parent_id' => 0,
+                'image_url' => 'https://minio.thecoffeehouse.com/image/admin/1677724504_ca-phe-uong-lien.png',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00'
+            ],
+            [
+                'id' => 8,
+                'name' => 'Thức uống khác',
+                'parent_id' => 0,
+                'image_url' => 'https://minio.thecoffeehouse.com/image/admin/1677724557_thuc-uong-khac.png',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00'
+            ],
+            [
+                'id' => 9,
+                'name' => 'Cà Phê Việt Nam',
+                'parent_id' => 1,
+                'image_url' => '',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00' 
+            ],
+            [
+                'id' => 10,
+                'name' => 'Cà Phê Máy',
+                'parent_id' => 1,
+                'image_url' => '',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00' 
+            ],
+            [
+                'id' => 11,
+                'name' => 'Trà trái cây',
+                'parent_id' => 4,
+                'image_url' => '',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00' 
+            ],
+            [
+                'id' => 12,
+                'name' => 'Trà sữa Macchiato',
+                'parent_id' => 4,
+                'image_url' => '',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00' 
+            ],
+            [
+                'id' => 13,
+                'name' => 'Bánh mặn',
+                'parent_id' => 6,
+                'image_url' => '',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00' 
+            ],
+            [
+                'id' => 14,
+                'name' => 'Snack',
+                'parent_id' => 6,
+                'image_url' => '',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00' 
+            ],
+            [
+                'id' => 15,
+                'name' => 'Cold Brew',
+                'parent_id' => 1,
+                'image_url' => '',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00' 
+            ],
+            [
+                'id' => 16,
+                'name' => 'Bánh ngọt',
+                'parent_id' => 6,
+                'image_url' => '',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00' 
+            ],
+            [
+                'id' => 17,
+                'name' => 'Cà phê tại nhà',
+                'parent_id' => 7,
+                'image_url' => '',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00' 
+            ],
+            [
+                'id' => 18,
+                'name' => 'Trà tại nhà',
+                'parent_id' => 7,
+                'image_url' => '',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00' 
+            ],
+            [
+                'id' => 19,
+                'name' => 'Chocolate',
+                'parent_id' => 8,
+                'image_url' => '',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00' 
+            ],
+            [
+                'id' => 20,
+                'name' => 'Hi-Tea Trà',
+                'parent_id' => 5,
+                'image_url' => '',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00' 
+            ],
+            [
+                'id' => 21,
+                'name' => 'Hi-Tea Đá Tuyết',
+                'parent_id' => 5,
+                'image_url' => '',
+                'created_at' => '2023-09-01 00:00:00',
+                'updated_at' => '2023-09-01 00:00:00' 
+            ],
+        ];  
+        category::truncate(); 
+        category::insert($data);
     }
 }
--- backend/database/seeders/Toppings.php
+++ backend/database/seeders/Toppings.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class Toppings extends Seeder
@@ -12,6 +11,34 @@ class Toppings extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = [
+            [
+                'id' => 1,
+                'name' => 'Kem Phô Mai Macchiato',
+                'price' => 10000,
+            ],
+            [
+                'id' => 2,
+                'name' => 'Shot Espresso',
+                'price' => 10000,
+            ],
+            [
+                'id' => 3,
+                'name' => 'Sốt Caramel',
+                'price' => 10000,
+            ],
+            [
+                'id' => 4,
+                'name' => 'Thạch Cà Phê',
+                'price' => 10000,
+            ],
+            [
+                'id' => 5,
+                'name' => 'Trân Châu Trắng',
+                'price' => 10000,
+            ],
+        ];
+        Topping::truncate();
+        Topping::insert($data);
     }
 }
